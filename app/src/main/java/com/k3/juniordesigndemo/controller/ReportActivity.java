a0beca1faddf4d3c90a52620bf3b9f8586398eca
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2019-04-14 18:51:51 -0400

    Added category

--- app/src/main/java/com/k3/juniordesigndemo/controller/ReportActivity.java
+++ app/src/main/java/com/k3/juniordesigndemo/controller/ReportActivity.java
@@ -3,8 +3,10 @@ package com.k3.juniordesigndemo.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.k3.juniordesigndemo.R;
@@ -12,6 +14,7 @@ import com.k3.juniordesigndemo.R;
 public class ReportActivity extends AppCompatActivity implements View.OnClickListener {
 
   // Activity components
+  private Spinner categorySpinner;
   private EditText addressEditText;
   private Button finishButton;
 
@@ -21,8 +24,16 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
     setContentView(R.layout.activity_report);
 
     // Get activity components
-    EditText addressEditText = findViewById(R.id.addressEditTextReportActivity);
-    Button finishButton = findViewById(R.id.finishButton);
+    categorySpinner = findViewById(R.id.categorySpinner);
+    addressEditText = findViewById(R.id.addressEditTextReportActivity);
+    finishButton = findViewById(R.id.finishButton);
+
+    // Populate spinner
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        this, R.array.categories_array, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    categorySpinner.setAdapter(adapter);
+
 
     // Set address from intent
     String address = getIntent().getStringExtra("ADDRESS");
