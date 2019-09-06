db3dde81df424f9798d8bdd3647300221cb9acb9
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2019-09-05 23:41:17 -0400

    Copied modifications from last semester and added initial paginated form activity

--- app/src/main/java/com/k3/juniordesigndemo/controller/PagedReportActivity.java
+++ app/src/main/java/com/k3/juniordesigndemo/controller/PagedReportActivity.java
@@ -1,15 +1,118 @@
 package com.k3.juniordesigndemo.controller;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.k3.juniordesigndemo.R;
 
 public class PagedReportActivity extends AppCompatActivity {
 
+  // Activity components
+  ViewPager viewPager;
+  LinearLayout dotsLinearLayout;
+  Button prevButton, nextButton;
+
+  int[] slides;
+
+  @SuppressLint("ClickableViewAccessibility")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_paged_report);
+
+    // Get activity components
+    viewPager = findViewById(R.id.viewPager);
+    dotsLinearLayout = findViewById(R.id.dotsLinearLayout);
+    prevButton = findViewById(R.id.previousButton);
+    nextButton = findViewById(R.id.nextButton);
+
+    // Set slides for view pager
+    slides = new int[] {
+        R.layout.fragment_street_issues_slide,
+        R.layout.fragment_street_issues_slide,
+        R.layout.fragment_street_issues_slide
+    };
+
+    // Disable swiping
+    viewPager.setOnTouchListener((v, event) -> true);
+
+    // Set viewPager's adapter
+    viewPager.setAdapter(new ScreenSlidePagerAdapter());
+
+    // Set initial dots to first page
+    updateNavigation(0);
+
+    // Set prev and next buttons' click listeners
+    prevButton.setOnClickListener(new PrevButtonOnClickListener());
+    nextButton.setOnClickListener(new NextButtonOnClickListener());
+  }
+
+  /**
+   * Updates the dots at the bottom of the screen by highlighting the correct dot
+   *
+   * @param currSlide The slide corresponding to the dot to be highlighted
+   */
+  private void updateNavigation(int currSlide) {
+    dotsLinearLayout.removeAllViews();
+    for(int i = 0; i < slides.length; i++) {
+      TextView dot = new TextView(this);
+      dot.setText(Html.fromHtml("&#8226;"));
+      dot.setTextSize(35);
+      dot.setTextColor(i == currSlide ? Color.LTGRAY : Color.DKGRAY);
+      dotsLinearLayout.addView(dot);
+    }
+    if(currSlide == 0) prevButton.setVisibility(View.INVISIBLE);
+    else if(currSlide == slides.length - 1) nextButton.setText("Submit");
+    else {
+      prevButton.setVisibility(View.VISIBLE);
+      nextButton.setText("Next");
+    }
+  }
+
+  private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+    public ScreenSlidePagerAdapter() {
+      super(getSupportFragmentManager());
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      return new StreetIssuesSlideFragment();
+    }
+
+    @Override
+    public int getCount() {
+      return slides.length;
+    }
+  }
+
+  private class PrevButtonOnClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+      int prev = Math.max(viewPager.getCurrentItem() - 1, 0);
+      viewPager.setCurrentItem(prev);
+      updateNavigation(prev);
+    }
+  }
+
+  private class NextButtonOnClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+      int next = Math.min(viewPager.getCurrentItem() + 1, slides.length - 1);
+      viewPager.setCurrentItem(next);
+      updateNavigation(next);
+    }
   }
 }
