92c634fc917ece89f32d0e2e397de02540203e21
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2019-11-06 17:08:41 -0500

    Changed input of some categories

--- app/src/main/java/com/k3/juniordesigndemo/controller/slides/HomeIssuesSlideFragment.java
+++ app/src/main/java/com/k3/juniordesigndemo/controller/slides/HomeIssuesSlideFragment.java
@@ -1,11 +1,16 @@
 package com.k3.juniordesigndemo.controller.slides;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.k3.juniordesigndemo.R;
 import com.k3.juniordesigndemo.controller.Singleton;
@@ -15,48 +20,43 @@ public class HomeIssuesSlideFragment extends MyFragment {
 
     CheckBox home_abandoned;
     CheckBox home_bars;
-    CheckBox home_broken_windows;
-    CheckBox home_condition;
+    EditText home_broken_windows;
+    Spinner home_condition;
     CheckBox home_disrepair;
     CheckBox home_renovated;
     CheckBox home_selling;
     CheckBox home_solar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         return inflater.inflate(R.layout.fragment_home_issues_slide, container, false);
     }
 
-
     public void onViewCreated (View view,
                                Bundle savedInstanceState) {
-
+        home_condition = view.findViewById(R.id.houseConditionSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+            R.array.intensity_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        home_condition.setAdapter(adapter);
         home_abandoned = view.findViewById(R.id.abandonedCheckbox);
         home_bars = view.findViewById(R.id.barsCheckbox);
-        home_broken_windows = view.findViewById(R.id.brokenWindowCheckbox);
+        home_broken_windows = view.findViewById(R.id.brokenWindowEditText);
         home_disrepair = view.findViewById(R.id.disrepairCheckbox);
-        home_condition = view.findViewById(R.id.houseConditionCheckbox);
         home_renovated = view.findViewById(R.id.renovationCheckbox);
         home_selling = view.findViewById(R.id.forSaleCheckbox);
         home_solar = view.findViewById(R.id.solarCheckbox);
-
-
-
-
     }
-
-
 
     @Override
     public void getBoxes() {
         Report currRep = Singleton.getReport();
         home_abandoned.setChecked(currRep.isHome_abandoned());
         home_bars.setChecked(currRep.isHome_bars());
-        home_broken_windows.setChecked(currRep.isHome_broken_windows());
+        home_broken_windows.setText(Integer.toString(currRep.isHome_broken_windows()));
         home_disrepair.setChecked(currRep.isHome_disrepair());
-        home_condition.setChecked(currRep.getHome_condition() == 1);
+        home_condition.setSelection(currRep.getHome_condition());
         home_renovated.setChecked(currRep.isHome_renovated());
         home_selling.setChecked(currRep.isHome_selling());
         home_solar.setChecked(currRep.isHome_solar());
@@ -67,9 +67,9 @@ public class HomeIssuesSlideFragment extends MyFragment {
         Report currRep = Singleton.getReport();
         currRep.setHome_abandoned(home_abandoned.isChecked());
         currRep.setHome_bars(home_bars.isChecked());
-        currRep.setHome_broken_windows(home_broken_windows.isChecked());
+        currRep.setHome_broken_windows(Integer.parseInt(home_broken_windows.getText().toString()));
         currRep.setHome_disrepair(home_disrepair.isChecked());
-        currRep.setHome_condition(home_condition.isChecked()? 1:0);
+        currRep.setHome_condition(home_condition.getSelectedItemPosition());
         currRep.setHome_renovated(home_renovated.isChecked());
         currRep.setHome_selling(home_selling.isChecked());
         currRep.setHome_solar(home_solar.isChecked());
--- app/src/main/java/com/k3/juniordesigndemo/controller/slides/YardIssuesSlideFragment.java
+++ app/src/main/java/com/k3/juniordesigndemo/controller/slides/YardIssuesSlideFragment.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Spinner;
 
 import com.k3.juniordesigndemo.R;
 import com.k3.juniordesigndemo.controller.Singleton;
@@ -13,32 +15,32 @@ import com.k3.juniordesigndemo.model.Report;
 
 public class YardIssuesSlideFragment extends MyFragment {
 
-    CheckBox landscaping;
+    Spinner landscaping;
     CheckBox abandoned_stuff;
     CheckBox backyard_junk;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         return inflater.inflate(R.layout.fragment_yard_issues_slide, container, false);
     }
 
     @Override
     public void onViewCreated (View view,
                                Bundle savedInstanceState) {
-        landscaping = view.findViewById(R.id.landscapingIssueCheckbox);
+        landscaping = view.findViewById(R.id.landscapingSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+            R.array.intensity_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        landscaping.setAdapter(adapter);
         abandoned_stuff = view.findViewById(R.id.frontTrashCheckbox);
         backyard_junk = view.findViewById(R.id.backTrashCheckbox);
-
-
     }
 
     @Override
     public void getBoxes() {
         Report currRep = Singleton.getReport();
-        landscaping.setChecked(currRep.isYard_landscaping());
+        landscaping.setSelection(currRep.getYard_landscaping());
         abandoned_stuff.setChecked(currRep.isYard_abandoned_junk());
         backyard_junk.setChecked(currRep.isYard_back_junk());
     }
@@ -46,7 +48,7 @@ public class YardIssuesSlideFragment extends MyFragment {
     @Override
     public void saveBoxes() {
         Report currRep = Singleton.getReport();
-        currRep.setYard_landscaping(landscaping.isChecked());
+        currRep.setYard_landscaping(landscaping.getSelectedItemPosition());
         currRep.setYard_abandoned_junk(abandoned_stuff.isChecked());
         currRep.setYard_back_junk(backyard_junk.isChecked());
     }
